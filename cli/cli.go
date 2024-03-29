7b99dc299198c567a4c1e117ee8961a01e07982b
Renol P. H. <reshifr@gmail.com>
2024-03-29 15:35:47 +0700

    fix: cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,14 +1,3 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/reshifr/secure-env/core/crypt"
-	"github.com/reshifr/secure-env/core/std"
-)
-
-func main() {
-	var h std.Argon2
-	kdf := crypt.NewKDF(h)
-	fmt.Println(kdf.PassphraseKey("Hello, CLI!", []byte{}, 16))
-}
+func main() {}
