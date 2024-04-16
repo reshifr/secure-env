86c5bcc2758605383469304378f26fb1103befcb
Renol P. H. <reshifr@gmail.com>
2024-04-16 21:01:41 +0700

    add: access index

--- core/env/impl/sqliteenv.go
+++ core/env/impl/sqliteenv.go
@@ -11,23 +11,23 @@ type SQLiteEnv struct {
 }
 
 func LoadSQLiteEnv(db *sql.DB) (*SQLiteEnv, error) {
-	const role = `
+	const roleTable = `
 		CREATE TABLE IF NOT EXISTS role (
 			id INTEGER PRIMARY KEY NOT NULL,
 			name VARCHAR(255) UNIQUE NOT NULL
 		)`
-	if _, err := db.Exec(role); err != nil {
+	if _, err := db.Exec(roleTable); err != nil {
 		return nil, err
 	}
-	const secret = `
+	const secretTable = `
 		CREATE TABLE IF NOT EXISTS secret (
 			id INTEGER PRIMARY KEY NOT NULL,
 			raw BLOB NOT NULL
 		)`
-	if _, err := db.Exec(secret); err != nil {
+	if _, err := db.Exec(secretTable); err != nil {
 		return nil, err
 	}
-	const access = `
+	const accessTable = `
 		CREATE TABLE IF NOT EXISTS access (
 			role_id INTEGER NOT NULL,
 			secret_id INTEGER NOT NULL,
@@ -36,7 +36,13 @@ func LoadSQLiteEnv(db *sql.DB) (*SQLiteEnv, error) {
 			FOREIGN KEY (secret_id) REFERENCES secret(id)
 				ON DELETE CASCADE ON UPDATE CASCADE
 		)`
-	if _, err := db.Exec(access); err != nil {
+	if _, err := db.Exec(accessTable); err != nil {
+		return nil, err
+	}
+	const accessIndex = `
+		CREATE INDEX IF NOT EXISTS access_index
+			ON access (role_id, secret_id)`
+	if _, err := db.Exec(accessIndex); err != nil {
 		return nil, err
 	}
 	env := &SQLiteEnv{db: db}
