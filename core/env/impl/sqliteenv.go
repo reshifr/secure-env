fd45cf4ec724b1a25765e333de7678f36d4e8a93
Renol P. H. <reshifr@gmail.com>
2024-04-16 20:34:05 +0700

    fix: sql syntax

--- core/env/impl/sqliteenv.go
+++ core/env/impl/sqliteenv.go
@@ -12,7 +12,7 @@ type SQLiteEnv struct {
 
 func LoadSQLiteEnv(db *sql.DB) (*SQLiteEnv, error) {
 	const role = `
-		CREATE TABLE IF NOT EXISTS role(
+		CREATE TABLE IF NOT EXISTS role (
 			id INTEGER PRIMARY KEY NOT NULL,
 			name VARCHAR(255) UNIQUE NOT NULL
 		)`
@@ -20,7 +20,7 @@ func LoadSQLiteEnv(db *sql.DB) (*SQLiteEnv, error) {
 		return nil, err
 	}
 	const secret = `
-		CREATE TABLE IF NOT EXISTS secret(
+		CREATE TABLE IF NOT EXISTS secret (
 			id INTEGER PRIMARY KEY NOT NULL,
 			raw BLOB NOT NULL
 		)`
@@ -31,7 +31,7 @@ func LoadSQLiteEnv(db *sql.DB) (*SQLiteEnv, error) {
 		CREATE TABLE IF NOT EXISTS access (
 			role_id INTEGER NOT NULL,
 			secret_id INTEGER NOT NULL,
-			FOREIGN KEY(role_id) REFERENCES role(id)
+			FOREIGN KEY (role_id) REFERENCES role(id)
 				ON DELETE CASCADE ON UPDATE CASCADE,
 			FOREIGN KEY (secret_id) REFERENCES secret(id)
 				ON DELETE CASCADE ON UPDATE CASCADE
